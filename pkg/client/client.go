976e9929b16c07942e174789435c3b3c50ce5d86
Graham Goudeau <graham.goudeau@solo.io>
2019-10-11 15:04:00 -0400

    Make sure to report usage immediately

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -2,17 +2,27 @@ package client
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"os"
 	"time"
 
-	api "github.com/solo-io/reporting-client/pkg/api/v1"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	api "github.com/solo-io/reporting-client/pkg/api/v1"
 )
 
 const (
 	// set this env var to the string "true" to prevent usage from being reported
-	DisableUsageVar  = "USAGE_REPORTING_DISABLE"
+	DisableUsageVar = "USAGE_REPORTING_DISABLE"
+	TestingUrl      = "localhost:8000"
+
 	errorSendTimeout = time.Second * 10
+
+	// careful setting values here- this will cause `StartReportingUsage` to potentially block for at least
+	// this long if there's no listener on the channel yet. In the case of glooctl, a long block at this point would be bad UX
+	initialSendTimeout = time.Millisecond * 100
 )
 
 // a type that knows how to load the usage payload you want to report
@@ -20,17 +30,38 @@ type UsagePayloadReader interface {
 	GetPayload() (map[string]string, error)
 }
 
-type ReportingServiceClientBuilder func() (api.ReportingServiceClient, error)
+// the type grpc.ClientConn is a struct- to make testing easier, alias it to this interface
+type CloseableConnection interface {
+	Close() error
+}
+
+type ReportingServiceClientBuilder func() (api.ReportingServiceClient, CloseableConnection, error)
 
 var defaultReportingServiceClientBuilder = func(url string) ReportingServiceClientBuilder {
-	return func() (api.ReportingServiceClient, error) {
-		clientConn, err := grpc.Dial(url, grpc.WithInsecure())
+	return func() (api.ReportingServiceClient, CloseableConnection, error) {
+		var clientConn *grpc.ClientConn
+		var err error
+
+		// for testing purposes
+		if url == TestingUrl {
+			clientConn, err = grpc.Dial(url, grpc.WithInsecure())
+		} else {
+			certPool, err := x509.SystemCertPool()
+			if err != nil {
+				return nil, nil, err
+			}
+			tlsConfig := &tls.Config{
+				RootCAs: certPool,
+			}
+			clientConn, err = grpc.Dial(url, grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)))
+		}
+
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 
 		client := api.NewReportingServiceClient(clientConn)
-		return client, nil
+		return client, clientConn, nil
 	}
 }
 
@@ -45,6 +76,7 @@ type client struct {
 	usagePayloadReader UsagePayloadReader
 	usageClientBuilder ReportingServiceClientBuilder
 	metadata           *api.InstanceMetadata
+	errorChan          chan error
 }
 
 var _ Client = &client{}
@@ -67,54 +99,63 @@ func newUsageClient(
 		usagePayloadReader: usagePayloadReader,
 		usageClientBuilder: reportingServiceClientBuilder,
 		metadata:           instanceMetadata,
+		errorChan:          make(chan error),
 	}
 }
 
 func (c *client) StartReportingUsage(ctx context.Context, interval time.Duration) <-chan error {
-	errorChan := make(chan error)
 	if os.Getenv(DisableUsageVar) == "true" {
-		close(errorChan)
-		return errorChan
+		close(c.errorChan)
+		return c.errorChan
 	}
+
+	// careful not to block this goroutine
+	go c.send(ctx, initialSendTimeout)
 
 	ticker := time.NewTicker(interval)
 	go func() {
 		for {
 			select {
 			case <-ticker.C:
-				payload, err := c.usagePayloadReader.GetPayload()
-				if err != nil {
-					sendWithTimeout(errorChan, ErrorReadingPayload(err))
-					continue
-				}
-
-				client, err := c.usageClientBuilder()
-				if err != nil {
-					sendWithTimeout(errorChan, ErrorConnecting(err))
-					continue
-				}
-				_, err = client.ReportUsage(ctx, &api.UsageRequest{
-					InstanceMetadata: c.metadata,
-					Payload:          payload,
-				})
-				if err != nil {
-					sendWithTimeout(errorChan, ErrorSendingUsage(err))
-				}
+				c.send(ctx, errorSendTimeout)
 			case <-ctx.Done():
-				close(errorChan)
+				close(c.errorChan)
 				return
 			}
 		}
 	}()
 
-	return errorChan
+	return c.errorChan
+}
+
+func (c *client) send(ctx context.Context, timeout time.Duration) {
+	payload, err := c.usagePayloadReader.GetPayload()
+	if err != nil {
+		sendWithTimeout(c.errorChan, ErrorReadingPayload(err), timeout)
+		return
+	}
+	client, conn, err := c.usageClientBuilder()
+	if err != nil {
+		sendWithTimeout(c.errorChan, ErrorConnecting(err), timeout)
+		return
+	} else {
+		defer conn.Close()
+	}
+	_, err = client.ReportUsage(ctx, &api.UsageRequest{
+		InstanceMetadata: c.metadata,
+		Payload:          payload,
+	})
+	if err != nil {
+		sendWithTimeout(c.errorChan, ErrorSendingUsage(err), timeout)
+		return
+	}
 }
 
 // we don't want to block this whole goroutine if no one is listening for errors,
 // so if a receiver isn't ready after the timeout, give up and continue
-func sendWithTimeout(errorChan chan<- error, err error) {
+func sendWithTimeout(errorChan chan<- error, err error, timeout time.Duration) {
 	select {
 	case errorChan <- err:
-	case <-time.After(errorSendTimeout):
+	case <-time.After(timeout):
 	}
 }
